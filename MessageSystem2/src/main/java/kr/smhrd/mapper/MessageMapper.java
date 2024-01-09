4c68c051bd0d4f34d612596eab1c495cfa376c03
kokosang11 <golddust312@gmail.com>
2024-01-09 12:40:51 +0900

    test

---
+++ MessageSystem2/src/main/java/kr/smhrd/mapper/MessageMapper.java
@@ -0,0 +1,24 @@
+package kr.smhrd.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.smhrd.entity.Message;
+import lombok.NonNull;
+
+@Mapper
+public interface MessageMapper {
+
+	public int messageSend(Message msg);
+
+	public List<Message> messageList(@NonNull String email);
+
+	public List<Message> messageList(int num);
+
+	public void deleteMessage(int num);
+
+	public void deleteMessageAll(@NonNull String email);
+
+
+}
